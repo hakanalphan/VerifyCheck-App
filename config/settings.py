d7d32fe0642af05dc604f8c196b3d74225bb8dcf
hakanalphan <78258929+hakanalphan@users.noreply.github.com>
2025-10-22 18:46:14 +0300

    uptade

--- config/settings.py
+++ config/settings.py
@@ -1,9 +1,8 @@
-# config/settings.py
 from pydantic_settings import BaseSettings
 from pydantic import Field
 
 class Settings(BaseSettings):
-    app_name: str = Field(default="KYC Doğrulama PoC")
+    app_name: str = Field(default="VerifyCheck")
     environment: str = Field(default="dev")
     allowed_extensions: set[str] = Field(default_factory=lambda: {".jpg", ".jpeg", ".png", ".bmp", ".tif", ".tiff"})
     min_name_similarity: int = 80
